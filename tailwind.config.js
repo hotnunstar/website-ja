425fdb723eb2b5e4491e90a902d02e00a6d22761
hotnunstar <nu-nos@hotmail.com>
2023-02-02 12:16:26 +0000

    index update

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/html/*.html"],
+  theme: {
+    screens:{
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors:{
+        darkPink: '#ffc8c8',
+        lightPink: '#feede8',
+        darkBrown: '#873e3e',
+        lightBrown: '#6a4c3c',
+        backgroundLines: '#f7e0d4',
+      }
+    },
+  },
+  plugins: [],
+}
